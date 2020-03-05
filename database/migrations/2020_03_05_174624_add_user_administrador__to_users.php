fee1eeb71d92e6dbe1e1036728a44c030f9bc055
benitezmvirginia <benitezmvirginia@gmail.com>
2020-03-05 15:03:04 -0300

    Agrego columna userAdministrador a db

---
+++ database/migrations/2020_03_05_174624_add_user_administrador__to_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserAdministradorToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //Agreago columna userAdministrador, es un booleano para saber si alguin puede subir preguntas o no.
+            $table->integer('userAdministrador')->after('avatar');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('userAdministrador');
+        });
+    }
+}
